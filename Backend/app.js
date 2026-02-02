5554c90ba04f32419e8dfffe0c88c1021d0c65d3
Ryann <154708766+Ryannds-dev@users.noreply.github.com>
2026-02-03 00:37:00 +0100

    Configure CORS to allow frontend production domain (from Vercel)

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-const path = require("path");
-require("dotenv").config();
 
 const bookRoutes = require("./routes/bookRouter");
 const userRoutes = require("./routes/userRouter");
@@ -17,10 +16,15 @@ mongoose
   .catch((err) => console.error("Connexion à MongoDB échouée :", err));
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://back-end-d-un-site-de-notation-de-livres-62rbx1fa7.vercel.app/",
+    ],
+  }),
+);
 app.use(express.json()); //pour lire req.body en JSON
-
-app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api/books", bookRoutes);
 app.use("/api/auth", userRoutes);
